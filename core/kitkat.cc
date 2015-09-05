92b3178af8d6328d2153c3eacbbdb1c8b2cb8bf7
Andy <Andymic12@gmail.com>
2015-09-05 19:21:16 -0400

    added ReadVideo, pointing to opencv_videoio as that module was moved in opencv 3.0, updated readme

--- core/kitkat.cc
+++ core/kitkat.cc
@@ -1,64 +1,95 @@
 //============================================================================
-// Name        : kitkat.cpp
+// Name        : kitkat.cc
 // Author      : Andy
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Version     : 1.0
+// Copyright   : Use this code at your own risk...your pants may fall off!
+// Description : OpenCV experimentation in C++, Ansi-style
 //============================================================================
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui_c.h>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio/videoio.hpp>
 #include <fstream>
+#include <string>
+#include <cstring>
 
 using namespace std;
 using namespace cv;
 
+#define img_path "/home/andy/Pictures/rockon.jpg"
+#define vid_path "/home/andy/Videos/Moving Takahashi-HD.mp4"
+
 class Opencv
 {
-public:
-  Opencv(){}
-  bool DoesFileExist(const char *fileName)
-  {
-      ifstream infile(fileName);
-      return infile.good();
-  }
-
-  int ShowImage(const char * path)
-  {
-
-    if(!DoesFileExist(path))
+    public:
+    Opencv(){}
+    bool DoesFileExist(const char *filename)
     {
-      cout<<"File not found"<<endl;
-      return (false);
+        ifstream infile(filename);
+        return infile.good();
     }
-
-    // create a MAT object for input image
-    Mat image;
-
-    // load an image
-    image = imread( path, CV_LOAD_IMAGE_COLOR );
-
-    if(!image.data )
+    int ShowImage(const char * path)
     {
-        cout<<" No image data"<<endl;
-        return (false);
+        if(!DoesFileExist(path))
+        {
+            cout<<"File not found"<<endl;
+            return (false);
+        }
+        // create a MAT object for input image
+        Mat image;
+        // load an image
+        image = imread( path, CV_LOAD_IMAGE_COLOR );
+        if(!image.data )
+        {
+            cout<<" No image data"<<endl;
+            return (false);
+        }
+        namedWindow( path, 1 );
+        // imshow() displays an image in the specified window.
+        // If the window was created with the CV_WINDOW_AUTOSIZE flag,
+        // the image is shown with its original size
+        imshow( path, image );
+        waitKey(0);
     }
+    void ReadVideo(const string & filename)
+    {
+      if(DoesFileExist(filename.c_str()))
+      {
+        const char * window_name = "kitkat";
+        VideoCapture capture(filename);
+ 
+        namedWindow(window_name, WINDOW_KEEPRATIO);
+        Mat frame;
 
-    namedWindow( path, 1 );
+        for(;;)
+        {
+          capture >> frame;
+          if(frame.empty())
+            break;
+
+          imshow(window_name, frame);
+
+          //following up each draw with waitKey so that
+          // highgui has time to process the draw request
+          char key = (char)waitKey(30);
+
+          if(key == 'q')
+            return;
+        }
 
 
-    // imshow() displays an image in the specified window. 
-    // If the window was created with the CV_WINDOW_AUTOSIZE flag, 
-    // the image is shown with its original size
-    imshow( path, image );
-    waitKey(0);
-  }
-  ~Opencv(){}
+      }
+      else
+      {
+        cout<<"File does not exist"<<endl;
+      }
+      
+    }
+    ~Opencv(){}
 };
-
-
 int main( int argc, char** argv )
 {
-  Opencv * c = new Opencv();
-  c->ShowImage("");
+    Opencv * c = new Opencv();
+    c->ReadVideo(vid_path);
     return 0;
 }
